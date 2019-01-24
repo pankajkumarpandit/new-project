31fc29d7c10cf6736af42ae31721d68ad864a116
pankajkumarpandit <46938405+pankajkumarpandit@users.noreply.github.com>
2019-01-24 23:11:30 +0530

    Add files via upload

---
+++ pankaj/js/custom.js
@@ -0,0 +1,7 @@
+//[Custom Javascript]
+
+//Project:	RealEstate - Onepage Html Responsive Template
+//Version:	1.1
+//Primary use:	RealEstate - Onepage Html Responsive Template 
+
+//add your script here
